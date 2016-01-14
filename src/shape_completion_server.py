67c5e932e867e21860f6400d98379ee37d0fbb37
avinair <asn2129@columbia.edu>
2016-01-14 11:41:22 -0500

    updated server

--- src/shape_completion_server.py
+++ src/shape_completion_server.py
@@ -6,12 +6,14 @@ import geometry_msgs.msg
 import shape_msgs.msg
 import rospy
 
+import argparse
+import subprocess
 import numpy as np
+from scipy.ndimage import map_coordinates
 import mcubes
 from experiments import reconstruction_3d_test
 from utils import reconstruction_utils
-import visualization.visualize as viz
-import pcl
+from off_utils import off_handler
 
 
 class ShapeCompletionServer(object):
@@ -19,14 +21,15 @@ class ShapeCompletionServer(object):
     _feedback = graspit_shape_completion.msg.CompleteMeshFeedback()
     _result = graspit_shape_completion.msg.CompleteMeshResult()
 
-    def __init__(self, name):
+    def __init__(self, name, smooth_mesh):
 
         rospy.loginfo('Loading model.')
 
-        weights_filepath = '/home/avinash/research/shape_completion/train/shape_completion_experiments/experiments/best_weights.h5'
+        weights_filepath = '/home/avinash/research/shape_completion/train/shape_completion_experiments/experiments/ycb_all_shrec_best_weights.h5'
         self.patch_size = 30
         self.model = reconstruction_3d_test.get_model()
         self.model.load_weights(weights_filepath)
+        self.smooth_mesh = smooth_mesh
 
         self._action_name = name
         self._as = actionlib.SimpleActionServer(self._action_name, graspit_shape_completion.msg.CompleteMeshAction,
@@ -34,6 +37,7 @@ class ShapeCompletionServer(object):
         self._as.start()
 
         rospy.loginfo('Node Initialized')
+
 
     def execute_cb(self, goal):
 
@@ -52,7 +56,7 @@ class ShapeCompletionServer(object):
 
         batch_x = np.zeros((1, self.patch_size, self.patch_size, self.patch_size, 1), dtype=np.float32)
 
-        batch_x[0, :, :, :, :] = reconstruction_utils.build_test_from_pc_scaled(pc, self.patch_size)
+        batch_x[0, :, :, :, :], voxel_resolution, offset = reconstruction_utils.build_test_from_pc_scaled(pc, self.patch_size)
 
         #make batch B2C01 rather than B012C
         batch_x = batch_x.transpose(0, 3, 4, 1, 2)
@@ -62,10 +66,38 @@ class ShapeCompletionServer(object):
 
         pred_as_b012c = pred.transpose(0, 3, 4, 1, 2)
 
-        v, t = mcubes.marching_cubes(pred_as_b012c[0, :, :, :, 0], 0.5)
-        #mcubes.export_mesh(v, t, 'results/' + 'model_' + str(1) + '.dae', 'model')
+        batch_x = batch_x.transpose(0, 3, 4, 1, 2)
+        mask = reconstruction_utils.get_occluded_voxel_grid(batch_x[0, :, :, :, 0])
+        completed_region = pred_as_b012c[0, :, :, :, 0] * mask
+
+        scale = 4
+        high_res_voxel_grid, voxel_resolution, offset = reconstruction_utils.build_high_res_voxel_grid(pc, scale, self.patch_size)
+        indices = np.mgrid[0:scale*self.patch_size:1, 0:scale*self.patch_size:1, 0:scale*self.patch_size:1]
+        scaled_completed_region = map_coordinates(completed_region, indices/scale, order = 0, mode = 'constant', cval=0.0)
+
+        output = high_res_voxel_grid[:, :, :, 0] + scaled_completed_region
+        #output = batch_x[0, :, :, :, 0] + completed_region
+
+        v, t = mcubes.marching_cubes(output, 0.25)
+        if self.smooth_mesh:
+            coarse_mesh = '/srv/data/temp/' + 'coarse.dae'
+            smooth_mesh = '/srv/data/temp/' + 'smooth.off'
+            script_file = '/srv/data/temp/' + 'poisson_remesh.mlx'
+            mcubes.export_mesh(v, t, coarse_mesh, 'model')
+            cmd_string = 'meshlabserver -i ' + coarse_mesh
+            cmd_string = cmd_string + ' -o ' + smooth_mesh
+            cmd_string = cmd_string + ' -s ' + script_file
+            process = subprocess.call(cmd_string, shell=True)
+            off = off_handler.OffHandler()
+            off.read(smooth_mesh)
+            v = off.vertices
+            t = off.faces
+
         #viz.visualize_batch_x(pred, 0, str(1), 'results/' + "pred_" + str(1))
         #viz.visualize_batch_x(batch_x, 0, str(1), 'results/' + "input_" + str(1))
+
+        v *= voxel_resolution
+        v += offset
 
         for i in range(len(v)):
             self._result.completed_mesh.vertices.append(geometry_msgs.msg.Point(v[i, 0], v[i, 1], v[i, 2]))
@@ -75,6 +107,18 @@ class ShapeCompletionServer(object):
         self._as.set_succeeded(self._result)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Completes the 3D shape of input point clouds')
+    parser.add_argument('--smooth',
+                        default=0,
+                        help='1: smooths the reconstructed mesh. 0: no smoothing is applied',
+                        type=int)
+    params = parser.parse_args()
+
+    if params.smooth:
+        rospy.loginfo('With Smoothing')
+    else:
+        rospy.loginfo('Without Smoothing')
+
     rospy.init_node('shape_completion_server')
-    ShapeCompletionServer('/complete_mesh')
+    ShapeCompletionServer('/complete_mesh', params.smooth)
     rospy.spin()
